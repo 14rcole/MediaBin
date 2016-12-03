56a4c8a37452a6477c53052ce8f2348603b96b1f
Ryan Cole <rcyoalne@gmail.com>
2016-12-03 08:55:32 -0500

    allow CORS

--- server.js
+++ server.js
@@ -11,6 +11,11 @@ var multer = require('multer');
 // configuration ===============================================================
 mongoose.connect(database.url); 	// connect to mongoDB database on modulus.io
 
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-Within")'
+}
+
 app.use(express.static(__dirname + '/public')); 		// set the static files location /public/img will be /img for users
 app.use(bodyParser.urlencoded({'extended':'true'})); // parse application/x-www-form-urlencoded
 app.use(bodyParser.json()); // parse application/json
